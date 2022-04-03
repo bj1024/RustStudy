96124866f9a5fe353f874a00a7d2795a82db3b49
H.K <16552133+bj1024@users.noreply.github.com>
2022-04-03 17:39:49 +0900

    rust loop.

--- exam01/src/main.rs
+++ exam01/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    another_function();
+
+
+    let mut a  = 100;
+    for cnt in 0..10{
+        println!("[{:3}]={:10}", cnt,a);
+        a += 1;
+    }
+
+}
+
+
+fn another_function() {
+    println!("Another function.");
 }
