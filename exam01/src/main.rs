48be54cf6b916bac2b55fe82a1e91e29191041da
H.K <16552133+bj1024@users.noreply.github.com>
2022-04-04 13:30:12 +0900

    add type_of

--- exam01/src/main.rs
+++ exam01/src/main.rs
@@ -1,11 +1,18 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
 fn main() {
     let mut x = 5;
-    println!("The value of x is: {}", x);
+    println!("The value of x is: {} {}", x,type_of(x));
     x = 6;
     println!("The value of x is: {}", x);
 
     another_function();
 
+    type_exam();
 
     let mut a  = 100;
     for cnt in 0..10{
@@ -14,6 +21,9 @@ fn main() {
     }
 
 }
+fn type_exam(){
+    println!("function typeExam");
+    }
 
 
 fn another_function() {
