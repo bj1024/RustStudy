e779ad39fdff5197a3c9617950e5838afb76e20b
H.K <16552133+bj1024@users.noreply.github.com>
2022-05-29 21:34:14 +0900

    file io.

---
+++ csvexam/src/main.rs
@@ -0,0 +1,57 @@
+use std::error::{Error};
+use std::fs::File;
+use std::{io, env};
+use std::io::{ErrorKind, Write};
+use std::process;
+
+fn example() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here.
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn filecheck(){
+    let filename = "hello.txt";
+
+    let f = File::options().append(true).open(filename);
+
+    let mut f: File  = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(filename) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+    println!("file opened.{:?}",filename);
+
+
+    f.write_all(b"Hello, world!");
+
+    f.sync_data();
+
+    
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    filecheck();
+    
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
+}
