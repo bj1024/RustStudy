d38513415b1b5e1230bb54a57cd23c5f6bbf7f3f
H.K <16552133+bj1024@users.noreply.github.com>
2022-06-23 12:17:46 +0900

    tokio::time

--- tokio_timer/src/main.rs
+++ tokio_timer/src/main.rs
@@ -4,11 +4,12 @@ use env_logger::Env;
 use log::{debug, error, info, trace, warn};
 
 use std::env;
+use std::env::args;
 use std::ffi::OsStr;
 use std::path::Path;
 use std::str::FromStr;
-use std::{env::args, time::Duration};
-use tokio::time::delay_for;
+// use std::time::Instant;
+use tokio::time::{interval, sleep, sleep_until, Duration, Instant};
 
 fn prog() -> String {
     env::args()
@@ -37,9 +38,52 @@ async fn main() {
 
     info!("start. [{}]", prog());
 
-    info!("delay_for start.");
-    delay_for(Duration::from_millis(100)).await;
-    info!("delay_for end.");
+    info!("tokio::time::sleep start.");
+    let start = Instant::now();
+    sleep(Duration::from_millis(1000)).await;
+    info!("tokio::time::sleep end. {}ms", start.elapsed().as_millis());
+
+    info!("tokio::time::sleep_until start.");
+    let start = Instant::now();
+    sleep_until(Instant::now() + Duration::from_millis(1000)).await;
+    info!(
+        "tokio::time::sleep_until end. {}ms",
+        start.elapsed().as_millis()
+    );
+
+    // interval は最後のinterval発生から指定時間が過ぎれば発生する。
+    // 初回はすぐ発生し、
+    info!("tokio::time::interval start.");
+    let start = Instant::now();
+    let mut interval = interval(Duration::from_millis(1000));
+
+    interval.tick().await; // 初回はすぐ発生。
+    info!(
+        "tokio::time::interval 1 end. {}ms",
+        start.elapsed().as_millis()
+    );
+
+    interval.tick().await; // 2回目は１回目interval終了＋1000msで発生。
+    info!(
+        "tokio::time::interval 2 end. {}ms",
+        start.elapsed().as_millis()
+    );
+
+    sleep(Duration::from_millis(500)).await; // 途中500msあけてみる。
+
+    interval.tick().await; // 前回のinterval＋1000msで発生するはず
+    info!(
+        "tokio::time::interval 3 end. {}ms",
+        start.elapsed().as_millis()
+    );
+
+    sleep(Duration::from_millis(1500)).await; // 途中1500msあけてみる。
+
+    interval.tick().await; // 途中1500ms経過(>1000)なので、すぐに発生する。
+    info!(
+        "tokio::time::interval 4 end. {}ms",
+        start.elapsed().as_millis()
+    );
 
     info!("fin. [{}]", prog());
 }
